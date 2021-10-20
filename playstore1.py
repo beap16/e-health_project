73ea73dd1ee8ef590bda3e0674d6770ab2f57823
AndreaSignorelli <andrea.signorelli@mail.polimi.it>
2021-10-20 11:04:18 +0100

    Solved search function limitations: now every query returns 50 games <3 Solved problem with Null values of scores and age ranges for some applications

--- playstore1.py
+++ playstore1.py
@@ -1,15 +1,16 @@
 import pandas as pd
 import json
+import ssl
 import play_scraper
 from google_play_scraper import app, Sort, reviews_all
 from tinydb import TinyDB
-
+##proviamo a fixare sta roba con ssl
+ssl._create_default_https_context = ssl._create_unverified_context
 
 # le librerie sono: play-scraper, google-play-scraper, google-play-scraper-py, pandas, tinydb
 
 def AddToDatabase(game_list, games_database):
     for games in game_list:
-        # print('yeah')
         url = games['url'].split('/store/apps/details?id=')
         url = url[1]
         goOn = True
@@ -24,8 +25,8 @@ def AddToDatabase(game_list, games_database):
                 country='us',  # defaults to 'us'
             )
             # print(result)
-
-            if(type(result['score']) is not None):
+            print(url)
+            if(type(result['score']) is float):
                 if (type(result['contentRating']) is not None):
                     if (result['score'] >= 4):
                         if(result['contentRating'] == "Everyone"):
@@ -40,7 +41,7 @@ def AddToDatabase(game_list, games_database):
             else:
                 print("score not found " + url)
 
-            return games_database
+    return games_database
 
 # Non ho usato il database che ci hanno dato in classe, ho usato subito le librerie online
 # Nonostante quello che dice nelle slide, non serve usare la libreria json per
@@ -67,21 +68,23 @@ all_games = []
 # uso la libreria play_scraper per cercare nell'intero database di google play quelli educativi (BISOGNA ESPANDERE STA
 # RICERCA IN MANIERA SENSATA)
 
-# keywords_general = ['serious game']
-keywords_general = ['serious game', 'game', 'children', 'educational', 'learning', 'learn', 'educative', 'family']
-keywords_onetime = ['adhd', 'dyslexia', 'hyperactivity', 'autism', 'rehab', 'sen', 'specific learning needs',
-                    'dyscalculia', 'Behaviour Emotional Social Difficulty', 'spastic', 'mentally retarded',
-                    'down syndrome']
+keywords_general = ['serious game']
+#keywords_general = ['serious game', 'game', 'children', 'educational', 'learning', 'learn', 'educative', 'family']
+#keywords_onetime = ['adhd', 'dyslexia', 'hyperactivity', 'autism', 'rehab', 'sen', 'specific learning needs',
+#                    'dyscalculia', 'Behaviour Emotional Social Difficulty', 'spastic', 'mentally retarded',
+#                    'down syndrome', 'motor']
+keywords_onetime = []
 # write a code that combines all the words, and then search for all the results
 
 for word in keywords_general:
-    games_list = play_scraper.search(word)
+    games_list = play_scraper.search(word, )
     # print(play_scraper.search(word))
     # this_game = games_list[0]
     # this_url = this_game['url'].split('/store/apps/details?id=')
     # print(play_scraper.similar(this_url[1]))
     # games_list.extend(play_scraper.similar(this_url[1]))
     # print(len(games_list))
+    print(len(games_list))
     AddToDatabase(games_list, all_games)
 
 print('\n')
@@ -96,25 +99,8 @@ for word in keywords_onetime:
 
 print(len(all_games))
 
-
-# ho caricato la lista all_games; ora trovo le ultime info necessarie (lo score dei giochi e l'age-range)
-"""for games in all_games:
-    result = app(
-        games.url,
-        lang='en',  # defaults to 'en'
-        country='us',  # defaults to 'us'
-    )
-    # print(result)
-    if(type(result['score']) != None and float(result['score'])>=4):
-        games.addRating(float(result['score']))
-    if(type(result['contentRating']) != None and result['contentRating'] == "Everyone"):
-        games.addAge(result['contentRating'])
-    print(games.title + '\n' + games.url + '\n' + str(games.rating) + '\n' + games.age_range + '\n\n')
-    # print(result['score'])
-
 # Now use pandas to manage your dataset
 # Possible ready-to-use alternatives is to use access, sql, mongoDB
 # We will use TinyDB, a document oriented database. It uses a simple
 
-db = TinyDB('./lab04DB.json')
-"""
+#db = TinyDB('./lab04DB.json')
